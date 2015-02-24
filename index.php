eaea7ef2a8e45945b8c088793205a377f51af14e
Dennis Hingst <post@dennis-hingst.de>
2015-02-24 14:24:15 +0100

    Remove AngularJS from plugin name

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Eddditor with AngularJS
+Plugin Name: Eddditor
 Description: Add and arrange your content freely with an intuitive drag and drop interface!
 Author: Dennis Hingst
 Version: 1.0
