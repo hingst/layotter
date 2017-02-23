6a1ed105e06bc2138d5ebe30c899ee1268e637b8
Dennis Hingst <post@dennis-hingst.de>
2017-02-23 17:39:14 +0100

    I broke something and now I fixed it :-)

--- assets/js/app/services/content.js
+++ assets/js/app/services/content.js
@@ -89,7 +89,7 @@ app.service('content', function($rootScope, $http, $animate, $timeout, data, for
 
         // build query string from form data
         var values = jQuery('#layotter-edit, .layotter-modal #post').serialize()
-            + '&layotter_element_id=' + encodeURIComponent(editingElement.id);
+            + '&layotter_element_id=' + encodeURIComponent(editingElement.id) + '&layotter_type=' + encodeURIComponent(editingElement.type);
         
         $http({
             url: ajaxurl + '?action=layotter_save_element',
