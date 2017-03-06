6ecd9f64806d8ee0ba99d7d3e578970ecaf28d5e
Dennis Hingst <post@dennis-hingst.de>
2017-03-06 20:17:46 +0100

    EditableMigrator can migrate elements and options

---
+++ core/Upgrades/EditableMigrator.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Layotter\Upgrades;
+
+use Layotter\Components\Editable;
+
+class EditableMigrator {
+
+    private $type;
+    private $fields = array();
+    private $values = array();
+
+    public function __construct($type, $allowed_fields, $provided_values) {
+        $this->type = strval($type);
+
+        if (is_array($allowed_fields)) {
+            $this->fields = $allowed_fields;
+        }
+
+        if (is_array($provided_values)) {
+            $this->values = $provided_values;
+        }
+    }
+
+    public function migrate() {
+        $id = wp_insert_post(array(
+            'post_type' => Editable::POST_TYPE_EDITABLE
+        ), true);
+        update_post_meta($id, Editable::META_FIELD_EDITABLE_TYPE, $this->type);
+
+        foreach ($this->fields as $field) {
+            $field_name = $field['name'];
+
+            // quick fix for broken Repeater fields
+            if ($field['type'] == 'repeater' && is_array($this->values[$field_name])) {
+                $values[$field_name] = array_values($this->values[$field_name]);
+            }
+
+            if (isset($this->values[$field_name])) {
+                $field['value'] = $this->values[$field_name];
+                update_field($field['key'], $field['value'], $id);
+            }
+        }
+
+        return $id;
+    }
+
+}
