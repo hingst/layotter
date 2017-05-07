b03e15d7536513bd77908ebabe4017f20f710c8a
Dennis Hingst <post@dennis-hingst.de>
2017-05-07 19:07:33 +0200

    configure phpunit to include ACF and run Layotter setup action

--- tests/test-something.php
+++ tests/test-something.php
@@ -2,14 +2,22 @@
 
 class DummyTest extends WP_UnitTestCase {
 
-	/**
-	 * A single example test.
-	 */
-	function test_sample() {
-		$post_id = $this->factory->post->create(array(
-		    'post_title' => 'some title'
+    function test_CanCreatePost() {
+        $post_id = self::factory()->post->create(array(
+            'post_title' => 'some title'
         ));
-		$title = get_the_title($post_id);
+        $title = get_the_title($post_id);
         $this->assertEquals($title, 'some title');
-	}
+    }
+
+    function test_AcfProIsAvailable() {
+        $this->assertTrue(\Layotter\Acf\Adapter::is_available());
+        $this->assertTrue(\Layotter\Acf\Adapter::is_pro_installed());
+    }
+
+    function test_DefaultSettingsAreAvailable() {
+        $settings = get_option('layotter_settings');
+        $this->assertInternalType('array', $settings);
+        $this->assertArrayHasKey('general', $settings);
+    }
 }
