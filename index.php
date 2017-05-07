22a7cca9fff6502c3f3b5c603f1ed6714784738c
Dennis Hingst <post@dennis-hingst.de>
2017-05-07 16:23:38 +0200

    Fixes #24

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Layotter
 Description: Add and arrange your content freely with an intuitive drag and drop interface!
 Author: Dennis Hingst
-Version: 1.5.16
+Version: 1.5.17
 Author URI: http://www.layotter.com/
 Text Domain: layotter
 GitHub Plugin URI: hingst/layotter
