790d275ece323a60facd23a07e65945109e79238
Dennis Hingst <post@dennis-hingst.de>
2017-08-13 19:38:06 +0200

    test layout migration

---
+++ tests/test-LayoutMigration.php
@@ -0,0 +1,32 @@
+<?php
+
+use \Layotter\Components\Post;
+use \Layotter\Upgrades\LayoutMigrator;
+
+class LayoutMigrationTest extends WP_UnitTestCase {
+
+    function test_CanMigrateLayout() {
+        update_option('layotter_post_layouts', [
+            0 => [
+                'layout_id' => 0,
+                'name' => 'cheese',
+                'json' => Layotter_Test_Data::POST_150_JSON,
+                'time_created' => 1502645077,
+            ],
+        ]);
+
+        $migrator = new LayoutMigrator(0);
+        $migrator->migrate();
+        $post = new Post();
+        $layouts = $post->get_available_layouts();
+
+        $this->assertArrayHasKey(0, $layouts);
+
+        $id = $layouts[0]->get_id();
+        $actual = $layouts[0]->get_frontend_view();
+
+        $model_version = get_post_meta($id, \Layotter\Upgrades\PluginMigrator::META_FIELD_MODEL_VERSION, true);
+        $this->assertEquals(Layotter_Test_Data::EXPECTED_VIEW, $actual);
+        $this->assertEquals(\Layotter\Upgrades\PluginMigrator::CURRENT_MODEL_VERSION, $model_version);
+    }
+}
