f37aa6eda24e1574d461fcd5404ecde99ef1da96
Dennis Hingst <post@dennis-hingst.de>
2019-01-11 22:16:45 +0100

    keep gulp watching on errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var concat = require('gulp-concat');
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var uglify = require('gulp-uglify');
 
 gulp.task('js', function (done) {
     gulp.src('assets/js/app/**/*.js')
+        .pipe(plumber())
         .pipe(uglify())
         .pipe(concat('app.min.js'))
         .pipe(gulp.dest('assets/js'));
@@ -14,12 +16,14 @@ gulp.task('js', function (done) {
 
 gulp.task('scss', function (done) {
     gulp.src('assets/css/editor.scss')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(concat('editor.min.css'))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('assets/css'));
     gulp.src('assets/css/frontend.scss')
+        .pipe(plumber())
         .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(concat('frontend.min.css'))
